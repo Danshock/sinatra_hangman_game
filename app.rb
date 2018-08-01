596d4bd35ae7e03fc0d99f4ded5d1fc95761e954
Danislav <danito1233@gmail.com>
2018-08-01 12:13:43 +0100

    I2: add functionality

---
+++ app.rb
@@ -0,0 +1,25 @@
+require "sinatra"
+#require "sinatra/reloader" if development?
+require_relative "lib/hangman.rb"
+
+enable :sessions
+
+get '/' do
+
+	session[:game] = Game.new if session[:game].nil?
+
+	if params["letter"] != nil
+	     session[:game].check(params["letter"])
+	end
+
+	if params[:restart] == "true"
+	     session[:game] = Game.new
+	end
+
+	erb :index, :locals => {:lives => session[:game].lives_left,
+		     	 		   :chosen_letters => session[:game].chosen_letters,
+		     	 		   :guessing => session[:game].display_word,
+		     	 		   :message => session[:game].status,
+		     	 		   :input_status => session[:game].input_status
+						  }
+end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,133 +1,83 @@
-require 'yaml'
+class Game
 
-class Hangman
-	attr_reader :word
-	attr_accessor :letters, :misses
+  attr_accessor :guessed, :guessing, :lives, :message, :status
+  attr_reader :to_guess
 
-	def initialize(word)
-		@word = word
-		@letters = ''
-		@misses = 0
-	end
-
-    def serialize
-    	YAML::dump(self)
-    end
-
-    def self.deserialize(yaml_string)
-    	YAML::load(yaml_string)
-    end
-
-    def hidden_word
-    	return '_' * @word.length if @letters == ''
-    	@word.gsub(/[^#{letters}]/, '_')
-    end
-
-    def won?
-    	!hidden_word.include?('_')
-    end
-
-    def lost?
-    	@misses >= 6
-    end
-
-    def guess(letter)
-    	if @word.include?(letter)
-    	  @letters += letter
-    	  true
-    	else
-    		@misses += 1
-    		@letters += letter
-    		false
-    	end
-    end
-end # of class Hangman
-
-def save_game(yaml_string)
-	File.open('save_file.txt', 'w') do |file|
-		file.puts yaml_string
-	end
-end
-
-DICTIONARY = File.open('5desk.txt', 'r').each_line.map do |line|
-	line.strip.downcase
-end.select { |word| (5..12).include?(word.length) }
-
-# Main Menu
-loop do
-	puts "\n\nWelcome to: "
-	puts "
- _   _                                         
-| | | |                                        
-| |_| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
-|  _  |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-\\_| |_/\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
-                    __/ |                      
-                   |___/                       
-		" 
-	puts "1 - New Game"
-	puts "2 - Load Game"
-
-	input = gets.chomp
-
-	case input
-	when '1'
-		game = Hangman.new(DICTIONARY.sample)
-	when '2'
-		if File.exist?('save_file.txt')
-			save_file = File.open('save_file.txt', 'r')
-			game = Hangman.deserialize(save_file)
-		else
-			puts 'Save file not found.'
-			next
-		end
-	 else
-	 	puts "Invalid input."
-	 	next
-	 end
-	
-loop do
-	puts ''
-	puts game.hidden_word
-	input = gets.chomp.downcase
-	
-	if input == 'quit' || input == 'exit'
-		puts "Bye!"
-		exit
-	elsif input == 'save'
-		save_game(game.serialize)
-		puts "Game saved."
-	elsif input.length == 1 && /[a-z]/ =~ input
-
-		if game.letters.include?(input)
-			puts "'#{input}' has been guessed already"
-			next
-		elsif game.guess(input)
-			puts "It does have a '#{input}'."
-		else
-			puts "It doesn't have a '#{input}'."
-		end
-
-	elsif input == game.word
-		puts "That's it! You won!"
-		break
-	else
-		puts "Invalid input."
-	end
-
-	if game.won?
-		puts game.word
-		puts "That's it! You won!"
-		break
-	elsif game.lost?
-		puts "You're out of guesses. You lost!"
-		puts "The word was: #{game.word}"
-		break
-	end
-
-	puts "Letters guessed so far: #{game.letters}"
-	puts "You've missed #{game.misses} times."
+# initialize the program
+  def initialize
+    @guessed = []
+    @to_guess = get_word
+    @guessing = Array.new(@to_guess.length, "_")
+    @lives = 10
+    @message = ""
+    @status = ""
   end
-end
 
+# load the list of word from the file, select the word between 5 & 12 chars
+# choose one word randomly from the array with sample
+  def get_word
+    words = File.readlines("lib/5desk.txt")
+    words.each { |word| word.chomp! }
+    words_list = words.select { |word| word.length.between?(5, 12)}
+
+    words_list.sample.downcase.split("")
+  end
+
+# check if the letter is in the word to guess
+  def check(guess)
+    correct = 0
+
+    if @guessed.include?(guess)
+      @message = "Choose another letter!"
+    else
+      @message = ""
+      @guessed << guess
+      @to_guess.each_with_index do |char, index|
+        if guess == char
+          @guessing[index] = guess
+          correct += 1
+        end
+      end
+      @lives -= 1 if correct == 0
+    end
+
+    if @guessing.join == @to_guess.join
+      @message = "#{@guessing.join} was the word. You won!!"
+      @status = "disabled"
+    end
+    if @lives == 0
+      @message = "No lives left! You lost! The word was: \"#{@to_guess.join}\""
+      @status = "disabled"
+    end
+
+  end
+
+# reset all the instance variables
+  def reset
+    @guessed = []
+    @to_guess = get_word
+    @guessing = Array.new(@to_guess.length, "_")
+    @lives = 10
+  end
+
+  def lives_left
+    @lives
+  end
+
+  def chosen_letters
+    @guessed.join(",")
+  end
+
+  def display_word
+    @guessing.join(" ")
+  end
+
+  def status
+    @message
+  end
+
+  def input_status
+    @status
+  end
+
+end
